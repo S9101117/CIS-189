12291c184ddbea2be56f5f546135c134a29a373a
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-07 11:03:29 +0500

    Add files via upload

--- test_functions.py
+++ test_functions.py
@@ -3,8 +3,8 @@ from main import camper_age_input
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(False, True)
+    def test_convert_to_months (self):
+        self.assertEqual(camper_age_input.convert_to_months(x),8)
 
 
 if __name__ == '__main__':
