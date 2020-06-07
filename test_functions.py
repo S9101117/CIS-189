279ec25076da674c23d2137d0d31fb6e04308141
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-07 10:21:11 +0500

    Add files via upload

---
+++ test_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from main import camper_age_input
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(False, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
