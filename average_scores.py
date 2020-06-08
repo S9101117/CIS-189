ecbe1dda49cdbb70c66bc81b1df1f0ebe0cc4296
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-08 17:36:16 +0500

    Add files via upload

--- average_scores.py
+++ average_scores.py
@@ -1,3 +1,17 @@
+#def average():
+ #   pass
+
+
+
+a = int(input('score1:'))
+b= int(input('score2:'))
+c= int(input('score3:'))
+def average(a,b,c):
+      return (int(a + b + c)/ 3)
+    # Get input for scores
+    # declare variables, use score1, score2, score3 in calculation
+
 if __name__ == '__main__':
-    def average():
-        pass
+    print(average(a,b,c))
+
+
--- test_average_scores.py
+++ test_average_scores.py
@@ -1,14 +1,14 @@
 import unittest
 from format_output import average_scores
 import unittest.mock as mock
-import input_output.topic2 as topic2
 
+from format_output.average_scores import a, b, c
 
 
 class MyTestCase(unittest.TestCase):
     def test_average(self):
-        with mock.patch('builtins.input', side_effect=[85,90,95]):
-            assert average_scores.average() == 90
+     with mock.patch ('builtins.input', side_effect=[85, 90, 95]):
+        assert average_scores.average(a,b,c)== 90
 
 
 
