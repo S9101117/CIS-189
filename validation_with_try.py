01ba1ea8d34531c6636f4d7eb36487e32ae6738e
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-12 20:17:08 +0500

    Add files via upload

--- validation_with_try.py
+++ validation_with_try.py
@@ -1,8 +1,23 @@
-def average(score1, score2, score3):
+def average( score1, score2, score3 ):
     NUMBER_TESTS = 3
-    if score3 < 0:
-        raise (ValueError)
-        return float((score1 + score2 + score3)/NUMBER_TESTS)
+    try:
+        if score1 < 0 or score2 < 0 or score3 < 0:
+            raise ValueError
+        else:
+            result = (score1 + score2 + score3) / NUMBER_TESTS
+            print("all positive")
+            return result
+
+    except ValueError as error:
+        return str(error)
+
+
+
+def main():
+    print(average(-90,80,30))
+
+if __name__=="__main__":
+    main()
 
 
 
