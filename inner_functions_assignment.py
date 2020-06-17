c77fb3cacae7b47cf496bc16d9cd9d6a5c958b89
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-18 02:48:53 +0500

    Add files via upload

--- inner_functions_assignment.py
+++ inner_functions_assignment.py
@@ -1,9 +1,16 @@
 def measurements(a_list):
     def area():
-        return "Area = " + str(a_list[0] * a_list[1])
+        if len(a_list) == 2:
+         return "Area = " + str(a_list[0] * a_list[1])
+        if len(a_list) == 1:
+         return "Area = " + str(a_list[0] * a_list[0])
     def perimeter():
-        return "Perimeter = " + str(2*(a_list[0] + a_list[1]))
+        if len(a_list) == 2:
+         return "Perimeter = " + str(2*(a_list[0] + a_list[1]))
+        if len(a_list) == 1:
+         return "Perimeter = " + str(2*(a_list[0] + a_list[0]))
     return str (perimeter() + ", " +area())
 if __name__ == '__main__':
-    a_list = [2.1,3.4]
+    a_list = [3.5]
     print(measurements(a_list))
+
