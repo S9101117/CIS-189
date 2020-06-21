5aae23ae30009f29a32705774f43d3bb56028d06
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-21 21:08:06 +0500

    Add files via upload

--- test_sort_and_search_list.py
+++ test_sort_and_search_list.py
@@ -3,11 +3,11 @@ from fun_with_collections import sort_and_search_list
 
 class MyTestCase( unittest.TestCase ):
     def test_item_found(self):
-        self.assertEqual(sort_and_search_list.search_list(listA,9), x )
+        self.assertEqual(sort_and_search_list.search_list(listA,9), 4)
 
 
     def test_item_not_found(self):
-        self.assertEqual(sort_and_search_list.search_list(listA,77), x)
+        self.assertEqual(sort_and_search_list.search_list(listA,77), -1)
 
 listA = [4, 3, 5, 1, 9, 3, 8 ]
 
