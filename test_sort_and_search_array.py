d87bfa54790b8b0059fbffd9031634caeb3e8b22
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-24 13:32:11 +0500

    Add files via upload

---
+++ test_sort_and_search_array.py
@@ -0,0 +1,18 @@
+import unittest
+from fun_with_collections import sort_and_search_array
+
+class MyTestCase( unittest.TestCase ):
+    def test_item_found(self):
+        self.assertEqual(sort_and_search_array.search_array(anyList,6), 5)
+
+
+    def test_item_not_found(self):
+        self.assertEqual(sort_and_search_array.search_array(anyList,33), -1)
+
+    def test_sort_list(self):
+        self.assertEqual(sort_and_search_array.sort_array(anyList),[1, 2, 3, 4, 5, 6, 7, 8])
+
+anyList = [5,4,6,3,7,2,8,1]
+
+if __name__ == '__main__':
+    unittest.main()
