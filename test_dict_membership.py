a06ef1d826777c9da11060e3b9553572363b7eb4
S9101117 <66375732+S9101117@users.noreply.github.com>
2020-06-25 03:08:24 +0500

    Add files via upload

--- test_dict_membership.py
+++ test_dict_membership.py
@@ -3,12 +3,12 @@ from more_fun_with_collections import dict_membership
 
 class test_in_set_true( unittest.TestCase ):
     def test_something(self):
-        self.assertEqual(dict_membership.in_dict(), True)
+        self.assertEqual(dict_membership.in_dict(mySet, 4), True)
 
     def test_in_set_true(self):
-        self.assertEqual(dict_membership.in_dict(), False)
+        self.assertEqual(dict_membership.in_dict(mySet, 8), False)
 
-
+mySet = set([4,3,2,1])
 
 
 if __name__ == '__main__':
--- test_set_membership.py
+++ test_set_membership.py
@@ -3,13 +3,13 @@ from more_fun_with_collections import set_membership
 
 class test_in_set_true( unittest.TestCase ):
     def test_something(self):
-        self.assertEqual(set_membership.in_set(), True)
+        self.assertEqual(set_membership.in_set(mySet, 4), True)
 
     def test_in_set_true(self):
-        self.assertEqual(set_membership.in_set(), False)
+        self.assertEqual(set_membership.in_set(mySet, 7), False)
 
 
-
+mySet = set([4,3,2,1])
 
 if __name__ == '__main__':
     unittest.main()
